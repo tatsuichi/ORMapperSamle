c47c68c67c88851884267f50b3a2bf02f4e379a3
tatsuichi <tatsuri1984@gmail.com>
2023-03-28 17:07:18 +0900

    DB操作の追加

--- WebApplicationDapper/Controllers/StudentsController.cs
+++ WebApplicationDapper/Controllers/StudentsController.cs
@@ -1,20 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using WebApplicationDapper.Models;
 
 namespace WebApplicationDapper.Controllers
 {
     public class StudentsController : Controller
     {
-        // GET: Students
-        public ActionResult Index()
+        private readonly string _connectionString;
+
+        public StudentsController(IConfiguration configuration)
         {
-            return View();
+            _connectionString = configuration.GetConnectionString("DbContext");
+        }
+
+        // GET: Students
+        public async Task<ActionResult> Index()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            
+            var sql = "SELECT * FROM Student";
+            
+            var students = await connection.QueryAsync<Student>(sql);
+
+            return View(students);
         }
 
         // GET: Students/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            using var connection = new SqlConnection(_connectionString);
+
+            var sql = "SELECT * FROM Student WHERE ID = @Id";
+
+            var student = connection.QueryFirstOrDefault<Student>(sql, new { Id = id });
+
+            return View(student);
         }
 
         // GET: Students/Create
@@ -26,10 +47,33 @@ namespace WebApplicationDapper.Controllers
         // POST: Students/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(IFormCollection collection)
         {
             try
             {
+                using var connection = new SqlConnection(_connectionString);
+
+                var sql = "INSERT INTO Student (FirstMidName, LastName, EnrollmentDate) VALUES (@FirstMidName, @LastName, @EnrollmentDate)";
+
+                if (!int.TryParse(collection["ID"].ToString(), out var id))
+                {
+                    return View();
+                }
+                if (!DateTime.TryParse(collection["EnrollmentDate"].ToString(), out var enrollmentDate))
+                {
+                    return View();
+                }
+
+                var student = new Student()
+                {
+                    ID = id,
+                    LastName = collection["LastName"].ToString(),
+                    FirstMidName = collection["FirstMidName"].ToString(),
+                    EnrollmentDate = enrollmentDate
+                };
+
+                var rowsAffected = await connection.ExecuteAsync(sql, student);
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -41,16 +85,41 @@ namespace WebApplicationDapper.Controllers
         // GET: Students/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            using var connection = new SqlConnection(_connectionString);
+
+            var sql = "SELECT * FROM Student WHERE ID = @Id";
+
+            var student = connection.QueryFirstOrDefault<Student>(sql, new { Id = id });
+
+            return View(student);
         }
 
         // POST: Students/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(int id, IFormCollection collection)
         {
             try
             {
+                using var connection = new SqlConnection(_connectionString);
+
+                var sql = "UPDATE Student SET FirstMidName = @FirstMidName, LastName = @LastName, EnrollmentDate = @EnrollmentDate WHERE ID = @Id";
+
+                if (!DateTime.TryParse(collection["EnrollmentDate"].ToString(), out var enrollmentDate))
+                {
+                    return View();
+                }
+
+                var student = new Student()
+                {
+                    ID = id,
+                    LastName = collection["LastName"].ToString(),
+                    FirstMidName = collection["FirstMidName"].ToString(),
+                    EnrollmentDate = enrollmentDate
+                };
+
+                var rowsAffected = await connection.ExecuteAsync(sql, student);
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -62,16 +131,33 @@ namespace WebApplicationDapper.Controllers
         // GET: Students/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            using var connection = new SqlConnection(_connectionString);
+
+            var sql = "SELECT * FROM Student WHERE ID = @Id";
+
+            var student = connection.QueryFirstOrDefault<Student>(sql, new { Id = id });
+
+            return View(student);
         }
 
         // POST: Students/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
         {
             try
             {
+                using var connection = new SqlConnection(_connectionString);
+
+                var sql = "DELETE FROM Student WHERE ID = @Id";
+
+                var student = new Student()
+                {
+                    ID = id,
+                };
+
+                var rowsAffected = await connection.ExecuteAsync(sql, student);
+
                 return RedirectToAction(nameof(Index));
             }
             catch
